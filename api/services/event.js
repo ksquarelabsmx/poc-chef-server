d8138c4041fe0e8a492f064d255a8013073d3609
Cald <aldo.archundia@ksquareinc.com>
2019-01-18 13:10:44 -0600

    Fixed: added curly braces, early-returns, removed one line if statements

--- api/services/event.js
+++ api/services/event.js
@@ -6,7 +6,7 @@
  * email: <ivan.sabido@ksquareinc.com>
  */
 
-const dataSource = require("../dataSource/dataSource");
+const dataSource = require("../data-source/data-source");
 const eventMocks = require("../utils/mocks/event");
 const { getTimeFromEpoch, getTimeFromMins } = require("../utils/time");
 
@@ -21,10 +21,10 @@ const getEvents = async () => {
 const getEvent = async req => {
   const eventId = req.params.eventId;
   const event = dataSource.events.find(event => event.id === eventId);
-  if (event) return Promise.resolve(event);
-  else {
-    return Promise.reject(new Error("That event Id did not match any event"));
+  if (event) {
+    return Promise.resolve(event);
   }
+  return Promise.reject(new Error("That event Id did not match any event"));
 };
 
 const markOneAsFinished = req => {
@@ -35,9 +35,8 @@ const markOneAsFinished = req => {
       const i = dataSource.events.indexOf(event);
       dataSource.events[i].finished = true;
       resolve(dataSource.events[i]);
-    } else {
-      reject("That event Id did not match any event");
     }
+    reject("That event Id did not match any event");
   });
 };
 
@@ -49,12 +48,12 @@ const createEvent = async ({ event }) => {
   let start_hour = getTimeFromMins(event.start_hour);
   let end_hour = getTimeFromMins(event.end_hour);
 
-  if (start_date < current_date)
+  if (start_date < current_date) {
     throw boom.badRequest("start date must not be less than current date.");
-
-  if (start_date >= expiration_date)
+  }
+  if (start_date >= expiration_date) {
     throw boom.badRequest("start date must be less than end date.");
-
+  }
   event.id = uuidv4();
 
   let newEvent = {
@@ -89,11 +88,13 @@ const updateEvent = async ({ event, id }) => {
   start_date.hour(start_hour.hour()).minute(start_hour.minute());
   expiration_date.hour(end_hour.hour()).minute(end_hour.minute());
   console.log(`start date: ${start_date}`);
-  if (start_date < current_date)
+  if (start_date < current_date) {
     throw boom.badRequest("start date must not be less than current date.");
+  }
   // validate that start date is less than end date
-  if (start_date >= end_date)
+  if (start_date >= end_date) {
     throw boom.badRequest("start date must be less than end date.");
+  }
   event.id = id;
   return Promise.resolve(event);
 };
