5021ed0cec8bf0a471db66b1177141e146fb80de
Miguel Delgado <miguel.delgado@ksquareinc.com>
2019-02-26 09:48:04 -0600

    refactor initial setup files

---
+++ api/app.ts
@@ -0,0 +1,67 @@
+import * as boom from "boom";
+import * as cors from "cors";
+import * as Debug from "debug";
+import * as helmet from "helmet";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+
+import { Request, Response } from "express";
+import {
+  logErrors,
+  wrapErrors,
+  clientErrorHandler,
+  errorHandler
+} from "./middlewares/errorHandlers";
+
+const routes = require("./routes/v1");
+const debug = Debug("chef:orders:app");
+const slash = require("express-slash");
+
+debug(`app loading...`);
+
+// express application
+const app = express();
+
+app.enable("strict routing");
+
+// allow cross origin requests
+// configure to only allow requests from certain origins
+app.use(cors());
+
+// secure express app, this adds some http headers for security.
+app.use(
+  helmet({
+    dnsPrefetchControl: false,
+    frameguard: false,
+    ieNoOpen: false
+  })
+);
+
+// parsing the request bodys
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+
+// routes
+routes(app);
+
+// v1/name/ redirect to v1/name
+app.use(slash());
+
+app.use(function(req: Request, res: Response) {
+  const {
+    output: { statusCode, payload }
+  } = boom.notFound();
+  res.status(statusCode).json(payload);
+});
+
+// handling errors
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
+
+export { app };
---
+++ config/index.ts
@@ -0,0 +1,79 @@
+// load .env file
+require("dotenv").config();
+
+import Debug from "debug";
+import chalk from "chalk";
+
+const debug = Debug("chef:orders:api:configuration");
+
+debug(
+  `Configuration API-Chef-Orders: ${chalk.magenta("getting configurations...")}`
+);
+
+const configurations: any = {
+  production: {
+    server: {
+      name: "production",
+      port: process.env.PORT
+    },
+    auth: {
+      username: process.env.AUTH_ADMIN_USERNAME,
+      password: process.env.AUTH_ADMIN_PASSWORD,
+      email: process.env.AUTH_ADMIN_EMAIL,
+      jwt_secret: process.env.AUTH_JWT_SECRET
+    }
+  },
+  testing: {
+    server: {
+      name: "testing",
+      port: process.env.PORT
+    }
+  },
+  development: {
+    server: {
+      name: "development",
+      port: process.env.PORT || "3000"
+    },
+    auth: {
+      username: process.env.AUTH_ADMIN_USERNAME,
+      password: process.env.AUTH_ADMIN_PASSWORD,
+      email: process.env.AUTH_ADMIN_EMAIL,
+      jwt_secret: process.env.AUTH_JWT_SECRET
+    },
+    db: {
+      database: process.env.DB_NAME || "poc_chef",
+      username: process.env.DB_USER || "ksquare",
+      password: process.env.DB_PASS || "ksquare",
+      host: process.env.DB_HOST || "localhost",
+      dialect: "postgres",
+      logging: (s: any) => debug(s),
+      pool: {
+        max: 10,
+        min: 0,
+        idle: 10000
+      },
+      query: {
+        raw: true
+      }
+    }
+  }
+};
+
+// environment: development, staging, testing, production
+const environment = process.env.NODE_ENV || "development";
+
+// return a object with environment configuration
+function setEnvironmentConfig(): any {
+  switch (environment) {
+    case "production":
+      return configurations["production"];
+    case "development":
+      return configurations["development"];
+    default:
+      return configurations["testing"];
+  }
+}
+
+const config = setEnvironmentConfig();
+
+export { config };
