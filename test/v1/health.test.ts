20225bdfe534933cca8e4b306d79428bb6962f04
Miguel Delgado <miguel.delgado@ksquareinc.com>
2019-03-08 09:51:10 -0600

    setup test

---
+++ test/v1/health.test.ts
@@ -0,0 +1,26 @@
+// Import the dev-dependencies
+import * as chai from "chai";
+import chaiHttp = require("chai-http");
+
+// Configure chaiHttp and should
+const should: Chai.Should = chai.should();
+chai.use(chaiHttp);
+
+import { server, healthURI } from "../common";
+
+describe("/health", () => {
+  it("Check api health", done => {
+    chai
+      .request(server)
+      .get(healthURI)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+
+        res.should.have.status(200);
+        res.body.should.have.property("message", "pong");
+        done();
+      });
+  });
+});
