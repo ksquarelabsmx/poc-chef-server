3794d1e7d3480ec4856e6aad1e2846aca119c632
Michell Ayala <starships@outlook.com>
2019-04-10 01:21:25 -0500

    fix: imports

--- api/user/repositories/index.ts
+++ api/user/repositories/index.ts
@@ -1,7 +1,7 @@
 import { OrdersRepository } from "./orders";
-import { ordersMemoryDataSource } from "api/common/data-sources/orders-memory-data-source";
-import { productsMemoryDataSource } from "api/common/data-sources/products-memory-data-source";
-import { eventsMemoryDataSource } from "api/common/data-sources/events-memory-data-source";
+import { ordersMemoryDataSource } from "../../common/data-sources/orders-memory-data-source";
+import { productsMemoryDataSource } from "../../common/data-sources/products-memory-data-source";
+import { eventsMemoryDataSource } from "../../common/data-sources/events-memory-data-source";
 import { EventsRepository } from "./events";
 
 const ordersRepository = OrdersRepository(
