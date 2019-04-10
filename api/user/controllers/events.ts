e3662e5d8a1a1fb62dba206579c57726560ff9c3
Michell Ayala <starships@outlook.com>
2019-04-10 03:28:48 -0500

    refactor: decouple controller from request/response objects

--- api/user/controllers/events.ts
+++ api/user/controllers/events.ts
@@ -1,16 +1,13 @@
+import { IEvent } from "../../common/models/event";
 import * as eventMapper from "../../common/mappers/event";
 
 export const EventsController = eventsRepository => {
-  const getEvents = async (req, res) => {
-    if (req.query.past !== undefined) {
-      const events = await eventsRepository.getPastEvents();
-      const eventsDto = events.map(eventMapper.toDto);
-      return res.json({ events: eventsDto });
+  const getEvents = async (query: any): Promise<IEvent[]> => {
+    if (query.getPastEvents) {
+      return eventsRepository.getPastEvents();
     }
 
-    const events = await eventsRepository.getCurrentEvents();
-    const eventsDto = events.map(eventMapper.toDto);
-    res.json({ events: eventsDto });
+    return eventsRepository.getCurrentEvents();
   };
 
   return {
