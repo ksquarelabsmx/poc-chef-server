dd1c3315c0a4f9adc5186d0bd3b673b04248d419
Michell Ayala <starships@outlook.com>
2019-04-10 04:45:45 -0500

    fix: query

--- api/user/controllers/events.ts
+++ api/user/controllers/events.ts
@@ -3,7 +3,7 @@ import * as eventMapper from "../../common/mappers/event";
 
 export const EventsController = eventService => {
   const getEvents = async (query: any): Promise<IEvent[]> => {
-    if (query.getPastEvents) {
+    if (query.pastEvents) {
       return eventService.getPastEvents();
     }
 
